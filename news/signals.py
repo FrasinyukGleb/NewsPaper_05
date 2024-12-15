c2bffa60869c9d03ee6332e1ffb0c8458230ff43
FrasinyukGleb <glebfrasinyuk@yandex.ru>
2024-12-15 18:52:37 +0300

    Upload of the update

---
+++ news/signals.py
@@ -0,0 +1,42 @@
+from django.db.models.signals import post_save, post_delete, m2m_changed
+from django.dispatch import receiver  # импортируем нужный декоратор
+from django.core.mail import mail_managers, EmailMultiAlternatives
+from django.template.loader import render_to_string
+from .models import Post, User
+
+
+
+def send_post_notification(post, subscribers):
+    for user in subscribers:
+        # Получаем наш html с учетом пользователя
+        html_content = render_to_string(
+            'post_created.html',
+            {
+                'post': post,
+                'user': user,
+            }
+        )
+
+        # Отправка письма
+        msg = EmailMultiAlternatives(
+            subject=f'{post.title} | {post.date_add.strftime("%Y-%m-%d")}',
+            body=post.text,
+            from_email='testovaya-p.ochta@yandex.ru',
+            to=[user.email],
+        )
+        msg.attach_alternative(html_content, "text/html")  # добавляем html
+        print(f'DEBUG: Sended email - {user.email}')
+        msg.send()  # отсылаем
+
+
+@receiver(m2m_changed, sender=Post)
+def new_post_notifier(sender, instance, **kwargs):
+    if kwargs['action'] == 'post_add':
+        categories = instance.category.all()
+        subscribers = []
+        for category in categories:
+            subscribers += category.subscribers.all()
+
+        subscribers = [s.email for s in subscribers]
+
+        send_post_notification(instance.preview(), instance.pk, instance.title, subscribers)
